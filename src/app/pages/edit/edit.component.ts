3109b2e570c433ce19483c79157e99684467d051
Natalia Herrera <natalia.herrera.rico@gmail.com>
2021-08-01 09:43:39 -0500

    image 2

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, AbstractControl } from '@angular/forms';
+import { FormControl, FormGroup, AbstractControl, RequiredValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -9,41 +9,62 @@ import { FormControl, FormGroup, AbstractControl } from '@angular/forms';
 })
 export class EditComponent implements OnInit {
   form: FormGroup;
-
-
+  public imageNew: { url: string, height: string, width: string };
+  public currentName
+  public currentImage;
 
   constructor() {
 
     this.form = new FormGroup({
-      image: new FormControl(''),
-      height: new FormControl(''),
-      width: new FormControl(''),
+      image: new FormControl('', [Validators.required]),
+      height: new FormControl('', [Validators.required]),
+      width: new FormControl('', [Validators.required]),
     });
-
 
   }
 
   onFileSelected(event) {
-    const newele = document.querySelector('.load');
-    const reader = new FileReader();
 
+    const reader = new FileReader();
+    this.currentName = event.target.files[0].name
 
     reader.addEventListener("load", () => {
 
-      sessionStorage.setItem('image', reader.result)
+      this.imageNew = {
+        url: reader.result as string,
+        height: '0',
+        width: '0'
+      }
+
+      sessionStorage.setItem(this.currentName, JSON.stringify(this.imageNew))
     })
 
     reader.readAsDataURL(event.target.files[0])
 
-    const recent = sessionStorage.getItem('image')
-    newele.setAttribute('src', recent)
   }
+
 
   get image() { return this.form.get('image'); }
   get height() { return this.form.get('height'); }
+  get width() { return this.form.get('width'); }
 
   submit() {
-    console.log(this.form.value);
+    const newele = document.querySelector('.load');
+    const sesion = sessionStorage.getItem(this.currentName)
+
+    sessionStorage.setItem(this.currentName, JSON.stringify({ url: JSON.parse(sesion).url, height: this.height.value, width: this.width.value }))
+
+
+    if (sesion) {
+      const recent = JSON.parse(sessionStorage.getItem(this.currentName)).url
+      const width = JSON.parse(sessionStorage.getItem(this.currentName)).width
+      const height = JSON.parse(sessionStorage.getItem(this.currentName)).height
+      if (recent) {
+        newele.setAttribute('src', recent)
+        newele.setAttribute('height', String(height))
+        newele.setAttribute('width', String(width))
+      }
+    }
   }
 
   ngOnInit(): void {
