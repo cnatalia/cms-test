22c612d637cf3ab31d344f9260377f78dc48720b
Natalia Herrera <natalia.herrera.rico@gmail.com>
2021-07-31 21:57:10 -0500

    image add

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,4 +1,6 @@
+/* tslint:disable */
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+
+
+  constructor() {
+
+    this.form = new FormGroup({
+      image: new FormControl(''),
+      height: new FormControl(''),
+      width: new FormControl(''),
+    });
+
+
+  }
+
+  onFileSelected(event) {
+    const newele = document.querySelector('.load');
+    const reader = new FileReader();
+
+
+    reader.addEventListener("load", () => {
+
+      sessionStorage.setItem('image', reader.result)
+    })
+
+    reader.readAsDataURL(event.target.files[0])
+
+    const recent = sessionStorage.getItem('image')
+    newele.setAttribute('src', recent)
+  }
+
+  get image() { return this.form.get('image'); }
+  get height() { return this.form.get('height'); }
+
+  submit() {
+    console.log(this.form.value);
+  }
 
   ngOnInit(): void {
+
+    this.form.get('height').valueChanges.subscribe((value) => { console.log(value) })
+
+
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PreviewComponent } from './preview/preview.component';
-import { EditComponent } from './edit/edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from '../app-routing.module';
+
+import { EditComponent } from './edit/edit.component';
+import { PreviewComponent } from './preview/preview.component';
 
 
 @NgModule({
   declarations: [PreviewComponent, EditComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule
   ]
 })
 export class PagesModule { }
