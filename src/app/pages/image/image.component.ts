582298667d1eceb22d7cdda2a3c3ff3df4fe5ff2
Natalia Herrera <natalia.herrera.rico@gmail.com>
2021-08-02 00:06:56 -0500

    preview complete

--- src/app/pages/image/image.component.ts
+++ src/app/pages/image/image.component.ts
@@ -6,8 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./image.component.scss']
 })
 export class ImageComponent implements OnInit {
-  @Input() src;
-  @Input() isRender = false;
+  @Input() src?;
+  @Input() isRender ? = false;
 
   // public isRender = false;
 
--- src/app/pages/text/text.component.ts
+++ src/app/pages/text/text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text.component.scss']
 })
 export class TextComponent implements OnInit {
+  @Input() text;
+  @Input() isRender = false;
 
   constructor() { }
 
